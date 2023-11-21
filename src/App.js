563440e98d2cafd7c2c9118b2b83a56b3c4f24a6
GOKULB2004 <151618856+GOKULB2004@users.noreply.github.com>
2023-11-21 16:55:19 +0530

    Add files via upload
    Question 4

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./index.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  // const [isActive,setIsActive] = useState(false)
+  let timer;
+  const handleStart = () => {
+    setCount(count + 1);
+    // setIsActive(true)
+  };
+
+  const handleStop = () => {
+    clearTimeout(timer);
+  };
+  const handlereset = () => {
+    setCount(0);
+    clearTimeout(timer);
+  };
+  useEffect(() => {
+    if (count) {
+      timer = setTimeout(handleStart, 1500);
+    }
+  }, [count]);
+
+  return (
+    <div className="App">
+      <h3>Time: {count} Seconds</h3>
+      <button onClick={handleStart}>start</button>
+      <button onClick={handleStop}>stop</button>
+      <button onClick={handlereset}>reset</button>
+    </div>
+  );
+}
